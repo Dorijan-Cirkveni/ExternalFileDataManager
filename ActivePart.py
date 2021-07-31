f238f397234b2f496c1562530a91f453e229ef66
Dorijan Cirkveni <dorijan.cirkveni@engineer.com>
2021-07-31 22:18:44 +0200

    Started work

---
+++ ActivePart.py
@@ -0,0 +1,9 @@
+class ActivePart:
+    def __init__(self):
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()
---
+++ Entity.py
@@ -0,0 +1,15 @@
+class Entity:
+    def __init__(self, tags):
+        self.tags = tags
+        self.active_parts
+        if "folder_content" in tags:
+
+        return
+
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()
