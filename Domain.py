314cf15d8e56c528df7d4f3af6e8f38542d4890a
Dorijan Cirkveni <dorijan.cirkveni@engineer.com>
2021-08-07 23:46:25 +0200

    ???

--- Domain.py
+++ Domain.py
@@ -1,3 +1,4 @@
+import os
 import re
 from typing import List
 from Entity import Entity
@@ -7,27 +8,36 @@ class Domain:
     def __init__(self, directory, entityList=None, depthLimit: int = 5):
         self.directory = directory
         self.indexCounter = 0
-        self.root=None
+        self.root = None
         self.entityList = entityList
         if entityList is not None:
-            self.rootEntity=entityList[0]
+            self.rootEntity = entityList[0]
             return
         self.entityList = dict()
         if type(depthLimit) != int:
             raise Exception("Depth limit must be a non-negative integer for a determined depth limit"
                             " or a negative integer for a non-existent one.")
+        while depthLimit!=0:
+        X = os.listdir(directory)
+        self.entityList = {e: Entity({"name": e}) for e in X}
+    def import_data(self,filename):
 
 
-    def raw_display(self, filter_crit: callable, sort_crit: callable):
-        L:List[Entity]=[e for e in self.entityList if filter_crit(e)]
+
+    def raw_display(self, filter_crit: callable = None, sort_crit: callable = None, depth=0):
+        if filter_crit is None:
+            filter_crit = lambda e, v: True
+        if sort_crit is None:
+            sort_crit = lambda x: 0
+        L: List[Entity] = [(e, v) for (e, v) in self.entityList.items() if filter_crit(e, v)]
         L.sort(key=sort_crit)
-        return str(e)
+        return "\n".join([e[0] for e in L])
 
 
 def main():
-    A = "12" \
-        "34"
-    print(A)
+    path = "C:\DDLC\my_mods\TMIWNHANDANHHTHTYVM\game"
+    X = Domain(path)
+    print(X.raw_display())
     return
 
 
